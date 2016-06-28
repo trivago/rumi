1bca0d131a8a5dd099c1c8ce035e75f8504ded36
jsacha <jakub.sacha@trivago.com>
2016-06-28 13:49:34 +0200

    Add convenient way to execute tests locally

---
+++ rumi.bat
@@ -0,0 +1,22 @@
+@ECHO off
+SETLOCAL EnableDelayedExpansion
+
+REM Current directory must be in Unix style and not Windows style for docker.
+SET PWD=%CD%
+
+REM Replace back slashes with slashes.
+SET PWD=%PWD:\=/%
+
+REM Remove the drive colon.
+SET PWD=%PWD::=%
+
+REM Lowercase the drive character, Unix file systems are case-sensitive.
+SET CHAR_LIST="AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+SET FIRST=!CHAR_LIST:*%PWD:~0,1%=!
+SET FIRST=!FIRST:~0,1!
+SET PWD=%FIRST%%PWD:~1%
+
+REM Add a slash to make it absolute.
+SET PWD=/%PWD%
+
+docker run -ti --rm -v %PWD%:/workdir -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /rumi/entrypoint_local trivago/rumi:dev %PWD%
--- rumi_local.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-docker run -ti -v %CD%:/workdir -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /rumi/entrypoint_local rumi:dev %CD%
